309ea0ad74c7f2454c2d35059a3eea37439d9a93
db1692 <donaldbarr22@gmail.com>
2021-12-07 15:54:41 +0000

    Adds many small style changes, and a credits section at the bottom

--- button.js
+++ button.js
@@ -1,11 +1,11 @@
 window.onload = function() {
 
-	var btn = document.querySelector( '.btn' );
+  var btn = document.querySelector('.btn');
 
-	var btnFront = btn.querySelector( '.btn-front' ),
-		btnNo = btn.querySelector( '.btn-back' );
+  var btnFront = btn.querySelector('.btn-front'),
+    btnYes = btn.querySelector('.btn-back .yes')
 
-	btnFront.addEventListener( 'click', function( event ) {
+		btnFront.addEventListener( 'click', function( event ) {
 		var mx = event.clientX - btn.offsetLeft,
 			my = event.clientY - btn.offsetTop;
 
@@ -22,18 +22,19 @@ window.onload = function() {
 		directions.sort( function( a, b ) {
 			return distance( mx, my, a.x, a.y ) - distance( mx, my, b.x, b.y );
 		} );
+      btn.setAttribute('data-direction', directions.shift().id);
+      btn.classList.add('is-open');
+    });
 
-		btn.setAttribute( 'data-direction', directions.shift().id );
-		btn.classList.add( 'is-open' );
-	} );
+  btnYes.addEventListener('click', function(event) {
+    btn.classList.remove('is-open');
+  });
 
-	btnNo.addEventListener( 'click', function( event ) {
-		btn.classList.remove( 'is-open' );
-	} );
 
-	function distance( x1, y1, x2, y2 ) {
-		var dx = x1-x2;
-		var dy = y1-y2;
-		return Math.sqrt( dx*dx + dy*dy );
-	}
+  function distance(x1, y1, x2, y2) {
+    var dx = x1 - x2;
+    var dy = y1 - y2;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
 };
